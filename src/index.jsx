cf01af0d008030fa59d6e1bfe26e162560cd9da9
Joey Brown <joseph.d.bronw94@gmail.com>
2022-02-24 13:35:40 -0500

    update packes to current versions

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
@@ -12,9 +12,9 @@ const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
